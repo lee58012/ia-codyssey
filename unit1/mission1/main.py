818fde073a1c19d378c6362d6e28212978527189
gian58012@gmail.com <gian58012@gmail.com>
2025-03-17 09:56:42 +0900

    Add Output Comparison

--- unit1/mission1/main.py
+++ unit1/mission1/main.py
@@ -3,9 +3,12 @@ print('Hello Mars')  # Hello Mars 출력
 try:   # 예외 처리
     with open('mission_computer_main.log', 'r', encoding='UTF-8') as f:
         lines = f.readlines()
+        for i in lines:  # 문제 발생 부분 출력
+            print(i)
         lines.reverse()  # 역순 정렬
-        
-    for i in lines:  # 문제 발생 부분 출력
+    
+    print("--------------역순 정렬 후--------------")
+    for i in lines:  #역순 정렬 후 출력
         print(i)
         
     with open('issue.txt', 'w', encoding='UTF-8') as f:  # 문제 발생 부분을 issue.txt파일로 저장
