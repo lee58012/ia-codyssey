76e80f3cf65e85b005ee78cf4236eef5473d7828
gian58012@gmail.com <gian58012@gmail.com>
2025-03-18 21:49:31 +0900

    add first print

--- unit1/mission2/main.py
+++ unit1/mission2/main.py
@@ -1,6 +1,7 @@
 try:   # 예외 처리
     with open('mission_computer_main.log', 'r', encoding='UTF-8') as f:    #파일 읽기
-        lines = f.readlines()
+        lines = f.readlines()                                              
+        print(lines)                                                       #출력
         lst = []
         
         for line in lines[1:]:                                             # 첫번째 줄 무시
@@ -12,10 +13,11 @@ try:   # 예외 처리
     lst.sort(key=lambda x: x[0], reverse=True)                            #리스트 연순 정렬
     print(lst)                                                            #역순 정렬 후 출력
     
-    mission_computer_main = {item[0]: item[1] for item in lst}            # 리스트를 사전 객체로 전환
+    my_dict = {item[0]: item[1] for item in lst}                          # 리스트를 사전 객체로 전환
+    
     
     with open('mission_computer_main.json', 'w') as f:                    #사전 객체를 json 파일로 저장
-        f.write(str(mission_computer_main))
+        f.write(str(my_dict))
 
     
 except Exception as e:  # 예외 처리
