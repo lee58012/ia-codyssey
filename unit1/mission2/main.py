f006c12ecebad309f9d2d036763452c2c1e5b8ef
gian58012@gmail.com <gian58012@gmail.com>
2025-03-19 17:23:45 +0900

    chage json format

--- unit1/mission2/main.py
+++ unit1/mission2/main.py
@@ -5,22 +5,22 @@ try:   # 예외 처리
         print(lines)                                                       
         lst = []
         
-        for line in lines[1:]:                                             # 첫번째 줄 무시
-            parts = line.rstrip().split(',')                               # 콤마를 기준으로 분류
-            lst.append([parts[0],parts[2]])                                # 리스트에 분류후 추가
+        for line in lines[1:]:                                            # 첫번째 줄 무시
+            parts = line.rstrip().split(',')                              # 콤마를 기준으로 분류
+            lst.append(parts)                                             # 리스트에 분류후 추가
         print("--------------리스트 출력--------------")
         print(lst)     # 리스트 객체 출력
         
     print("--------------역순 정렬 후--------------")
     lst.sort(key=lambda x: x[0], reverse=True)                            #리스트 연순 정렬
     print(lst)                                                            #역순 정렬 후 출력
-    
-    my_dict = {item[0]: item[1] for item in lst}                          # 리스트를 사전 객체로 전환
-    
+    dict_list = []
+    for item in lst:
+        dict_list.append({"timestamp": item[0], "event":item[1], "message":item[2]})
+                                                                # 리스트를 사전 객체로 전환후 리스트에 저장
     
     with open('mission_computer_main.json', 'w') as f:                    #사전 객체를 json 파일로 저장
-        f.write(str(my_dict))
-
+        f.write(str(dict_list))
     
 except Exception as e:  # 예외 처리
     print(e)
