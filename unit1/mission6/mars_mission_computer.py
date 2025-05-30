cec49db255820562dbab4031547ec8b4d5bc267c
gian58012@gmail.com <gian58012@gmail.com>
2025-05-30 18:41:56 +0900

    1

---
+++ unit1/mission6/mars_mission_computer.py
@@ -0,0 +1,36 @@
+# mars_mission_computer.py
+
+import random
+
+
+class DummySensor:
+    #화성 기지 환경 센서 더미 클래스
+
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': 0.0,
+            'mars_base_external_temperature': 0.0,
+            'mars_base_internal_humidity': 0.0,
+            'mars_base_external_illuminance': 0,
+            'mars_base_internal_co2': 0.0,
+            'mars_base_internal_oxygen': 0.0
+        }
+
+    def set_env(self):
+        #환경 값을 무작위로 생성하여 env_values에 저장
+        self.env_values['mars_base_internal_temperature'] = round(random.uniform(18, 30), 1)
+        self.env_values['mars_base_external_temperature'] = round(random.uniform(0, 21), 1)
+        self.env_values['mars_base_internal_humidity'] = round(random.uniform(50, 60), 1)
+        self.env_values['mars_base_external_illuminance'] = random.randint(500, 715)
+        self.env_values['mars_base_internal_co2'] = round(random.uniform(0.02, 0.1), 3)
+        self.env_values['mars_base_internal_oxygen'] = round(random.uniform(4, 7), 1)
+
+    def get_env(self):
+        #현재 환경 값을 반환
+        return self.env_values
+
+
+if __name__ == '__main__':
+    ds = DummySensor()
+    ds.set_env()
+    print(ds.get_env())
